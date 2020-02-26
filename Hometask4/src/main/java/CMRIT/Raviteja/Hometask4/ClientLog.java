f435722d1d22ac77a321a78c6e107aef3daa1fda
Raviteja77 <51739120+Raviteja77@users.noreply.github.com>
2020-02-26 23:10:44 +0530

    Epam Hometask 4

---
+++ Hometask4/src/main/java/CMRIT/Raviteja/Hometask4/ClientLog.java
@@ -0,0 +1,11 @@
+package CMRIT.Raviteja.Hometask4;
+
+public class ClientLog extends InterestCalculatorLog{
+
+	public static void main(String[] args) {
+		InterestCalculatorLog i = new InterestCalculatorLog();
+		i.Interest();
+
+	}
+
+}
