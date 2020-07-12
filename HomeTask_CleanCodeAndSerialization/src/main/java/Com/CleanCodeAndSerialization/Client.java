df0fe4e6e26b67f96ddd9ff73fbe0934300226d6
Raviteja77 <51739120+Raviteja77@users.noreply.github.com>
2020-07-12 14:44:35 +0530

    Delete Client.java

--- HomeTask_CleanCodeAndSerialization/src/main/java/Com/CleanCodeAndSerialization/Client.java
+++
@@ -1,9 +0,0 @@
-package Com.CleanCodeAndSerialization;
-
-public class Client extends InterestCalculator{
-	public static void main(String args[]) {
-		InterestCalculator i = new InterestCalculator();
-		i.Interest();
-	}
-
-}
